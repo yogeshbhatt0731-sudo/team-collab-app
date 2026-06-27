b55b6cf1007bfc90b0abd7defd1fdef585a64cbc
Yogesh <yogesh.bhatt0731@gmail.com>
2026-06-27 15:30:37 +0530

    workspace_service initial setup/skeleton

---
+++ workspace-service/src/test/java/com/teamcollab/workspaceservice/WorkspaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamcollab.workspaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
