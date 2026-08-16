3c112df449836190e89387c8d03d2c296fcce588
Yogesh <yogesh.bhatt0731@gmail.com>
2026-08-16 14:51:49 +0530

    changed llm model to qwen-qwq-32b

--- ai-service/app/rag.py
+++ ai-service/app/rag.py
@@ -9,7 +9,7 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="qwen-qwq-32b",
     temperature=0.3,
     api_key=GROQ_API_KEY
 )
